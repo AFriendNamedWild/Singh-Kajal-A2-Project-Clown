2c4eee6fa5f7946f22b9e02b6d230ebebd2f615d
Kajal <kkrsp00@gmail.com>
2026-09-24 12:43:32 -0400

    Clown

--- A2-Project-Clown/Game.cs
+++ A2-Project-Clown/Game.cs
@@ -11,7 +11,10 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
+        Color color = Random.Color();
 
+        Color NoseColor = new Color(250,0,0);
+        Color HairColor = new Color(0, 250, 250);
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -20,8 +23,7 @@ namespace MohawkGame2D
         {
             Window.SetTitle("Meet Mr Chuckles!");
             Window.SetSize(600,600);
-
-            Draw.SetLineSize(2);
+            Draw.SetLineSize(5);
 
         }
 
@@ -30,8 +32,61 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(240);
+            Draw.SetLineSize(0);
 
+            //Hair
+            Draw.SetFillColor(HairColor);
+            Draw.Circle(300, 210, 255);
+
+
+            //Head
+            Draw.SetFillColor(255,255,255);
+            Draw.Circle(300,300,200);
+
+            //Bangs
+            Draw.SetFillColor(HairColor);
+            Draw.Circle(300,140,70);
+            Draw.Circle(80, 360,35);
+            Draw.Circle(580, 260, 45);
+            Draw.Circle(450, 160, 30);
+            Draw.Circle(100, 178, 90);
+            Draw.Circle(520, 70, 50);
+
+            //Eyes
+            Draw.SetFillColor(0,0,0);
+            Draw.Circle(230,250,35);
+            Draw.Circle(365,250,35);
+
+            //Keyboard Input???? For Expression change
+
+
+
+
+            //Nose
+            Draw.SetFillColor(NoseColor);
+            Draw.Circle(300, 340, 50);
+
+
+
+            //Mouse Input for Nose Interraction
+
+            if (Input.IsMouseButtonPressed(MouseButton.Left) == true)
+            {
+                NoseColor = Random.Color();
+            }
+
+
+            //Mouse Input for Hair Color
+
+            if (Input.IsMouseButtonPressed(MouseButton.Left) == true)
+            {
+                HairColor = Random.Color();
+            }
         }
+
+
+
     }
 
 }
